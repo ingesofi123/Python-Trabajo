04ffbe77b4041dfd482daed4ee6fc4ed442a8c9c
juanDcuatindioyN <126802980+juanDcuatindioyN@users.noreply.github.com>
2024-04-18 11:42:37 -0500

    funcionando

--- proyectos python/energia.py
+++ proyectos python/energia.py
@@ -13,28 +13,28 @@ while True:
     opcion = int(input("Ingrese la opcion que desea visualizar: "))
 
     if opcion == 1:
-        print("Usted ah seleccionado la opcion 1 de ingresar una nueva escritura :)")
+        print("Usted ha seleccionado la opcion 1: Ingresar una nueva escritura :")
         print("--------------------------------------------------------------------")
         lectura = float(input("Ingrese la lectura que desee: "))
         lecturas.append(lectura)
     elif opcion == 2:
-        print("Usted ah seleccionado la opcion 2 de calcular el promedio de lecturas :)")
+        print("Usted ha seleccionado la opcion 2: Calcular el promedio de lecturas :")
         print("--------------------------------------------------------------------")
         promedio = sum(lecturas) / len(lecturas)
         print("Promedio de lecturas que desee calcular: ", promedio)
     elif opcion == 3:
-        print("Usted ah seleccionado la opcion 3 de mostrar la lectura maxima)")
+        print("Usted ha seleccionado la opcion 3 de mostrar la lectura maxima: ")
         print("--------------------------------------------------------------------")
         print("Lectura máxima que usted ingreso es: ", max(lecturas))
     elif opcion == 4:
-        print("Usted ah seleccionado la opcion 4 de mostrar la lectura minima)")
+        print("Usted ha seleccionado la opcion 4: Mostrar la lectura minima: ")
         print("--------------------------------------------------------------------")
         print("Lectura mínima que usted ingreso es: ", min(lecturas))
     elif opcion == 5:
-        print("Usted ah seleccionado la opcion 5 la cual muestra la cantidad de lecturas realizadas)")
+        print("Usted ha seleccionado la opcion 5: Muestra la cantidad de lecturas realizadas: ")
         print("--------------------------------------------------------------------")
         print("Cantidad de lecturas que usted realizo son: ", len(lecturas))
     elif opcion == 6:
         break
     else:
-        print("Ups! esa opción es inválida... Por favor, intente de nuevo :) ")
+        print("Ups! esa opción es inválida... Por favor, intente de nuevo : ")
