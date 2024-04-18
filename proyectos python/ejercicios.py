df352750c7a900158170c28fa1482e2afdc94d8f
juanDcuatindioyN <126802980+juanDcuatindioyN@users.noreply.github.com>
2024-04-18 11:48:05 -0500

    pequeños cambios JAJAJA

--- proyectos python/ejercicios.py
+++ proyectos python/ejercicios.py
@@ -1,13 +1,13 @@
 while True:
     print("\nAutores: Sofia Burbano - Juan Cuatindioy")
-    print("\nSelecciona una Opcion :)")
     print("1. Lecturas de energia electrica: ")
     print("2. Sumatoria de divisores primos: ")
     print("3. Numeros curiosos ")
     print("4. salir")
-    opcion= int(input("Seleccione lo que quiere realizar"))
+    opcion= int(input("\nSelecciona una Opcion :\n "))
     if opcion == 1:
-        print("\nSelecciona una Opcion :)")
+        lecturas= []
+        print("\nSelecciona una Opcion :\n")
         print("1. Para ingresar una nueva lectura")
         print("2. Para calcular promedio de lecturas")
         print("2. Para calcular promedio de lecturas")
@@ -17,31 +17,32 @@ while True:
         print("6. Para Salir")
         op = int(input("Ingrese la opcion que desea visualizar: "))
         if op == 1:
-            print("Usted ah seleccionado la opcion 1 de ingresar una nueva escritura :)")
+            print("Usted ha seleccionado la opcion 1: Ingresar una nueva escritura :")
             print("--------------------------------------------------------------------")
             lectura = float(input("Ingrese la lectura que desee: "))
-            lectura.append(lectura)
+            lecturas.append(lectura)
         elif op == 2:
-            print("Usted ah seleccionado la opcion 2 de calcular el promedio de lecturas :)")
+            print("Usted ha seleccionado la opcion 2: Calcular el promedio de lecturas :")
             print("--------------------------------------------------------------------")
-            promedio = sum(lectura) / len(lectura)
+            promedio = sum(lecturas) / len(lecturas)
             print("Promedio de lecturas que desee calcular: ", promedio)
         elif op == 3:
-            print("Usted ah seleccionado la opcion 3 de mostrar la lectura maxima)")
+            print("Usted ha seleccionado la opcion 3 de mostrar la lectura maxima: ")
             print("--------------------------------------------------------------------")
-            print("Lectura máxima que usted ingreso es: ", max(lectura))
+            print("Lectura máxima que usted ingreso es: ", max(lecturas))
         elif op == 4:
-            print("Usted ah seleccionado la opcion 4 de mostrar la lectura minima)")
+            print("Usted ha seleccionado la opcion 4: Mostrar la lectura minima: ")
             print("--------------------------------------------------------------------")
-            print("Lectura mínima que usted ingreso es: ", min(lectura))
+            print("Lectura mínima que usted ingreso es: ", min(lecturas))
         elif op == 5:
-            print("Usted ah seleccionado la opcion 5 la cual muestra la cantidad de lecturas realizadas)")
+            print("Usted ha seleccionado la opcion 5: Muestra la cantidad de lecturas realizadas: ")
             print("--------------------------------------------------------------------")
-            print("Cantidad de lecturas que usted realizo son: ", len(lectura))
+            print("Cantidad de lecturas que usted realizo son: ", len(lecturas))
         elif op == 6:
             break
         else:
-            print("Ups! esa opción es inválida... Por favor, intente de nuevo :) ")
+            print("Ups! esa opción es inválida... Por favor, intente de nuevo : ")
+    
     elif opcion == 2:
         def esPrimo(n):
             if n < 2:
@@ -61,3 +62,8 @@ while True:
         print("----------------------------------------------------------------------")
         numero = int(input("Ingrese un número que desees sumar: "))
         print("La sumatoria de los divisores primos del numero que digito es: ", sumaDivisores(numero), "NUMEROS")
+    elif opcion==4:
+        print("has salido. Vuelve pronto... ")
+        break
+    else:
+        print("ninguna opcion es valida: ")
